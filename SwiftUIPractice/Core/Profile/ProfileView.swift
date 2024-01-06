1cb84499645fde7cfee220a4883b81e789706948
suojae3 <suojae3@gmail.com>
2024-01-06 22:52:41 +0900

    feat: GridView 추가

--- SwiftUIPractice/Core/Profile/ProfileView.swift
+++ SwiftUIPractice/Core/Profile/ProfileView.swift
@@ -8,58 +8,73 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    private let gridItems: [GridItem] = [
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1),
+        .init(.flexible(), spacing: 1)
+    ]
+    
     var body: some View {
-        VStack {
-            // header
-            VStack(spacing: 10) {
-                // pic and stats
-                HStack {
-                    Image("Sessac")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 80, height: 80)
-                        .clipShape(Circle())
+        ScrollView {
+            VStack {
+                // header
+                VStack(spacing: 10) {
+                    // pic and stats
+                    HStack {
+                        Image("Sessac")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 80, height: 80)
+                            .clipShape(Circle())
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 8) {
+                            UserStatView(value: 10, title: "Posts")
+                            UserStatView(value: 80, title: "Followers")
+                            UserStatView(value: 76, title: "Following")
+                        }
+                    }
+                    .padding(.horizontal)
                     
-                    Spacer()
+                    // name and bio
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Suojae")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Text("SwiftUI 꿀잼이였네")
+                            .font(.footnote)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
                     
-                    HStack(spacing: 8) {
-                        UserStatView(value: 10, title: "Posts")
-                        UserStatView(value: 80, title: "Followers")
-                        UserStatView(value: 76, title: "Following")
+                    // action Button
+                    Button {
+                        
+                    } label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundColor(.black)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color.gray, lineWidth: 1))
+                    }
+                    Divider()
+                }
+                
+                // post grid view
+                LazyVGrid(columns: gridItems, spacing: 1) {
+                    ForEach(0...15, id: \.self) { inedx in
+                        Image("Book")
+                            .resizable()
+                            .scaledToFill()
                     }
                 }
-                .padding(.horizontal)
-                
-                // name and bio
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("Suojae")
-                        .font(.footnote)
-                        .fontWeight(.semibold)
-                        
-                    Text("SwiftUI 꿀잼이였네")
-                        .font(.footnote)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-                
-                // action Button
-                Button {
-                    
-                } label: {
-                    Text("Edit Profile")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .frame(width: 360, height: 32)
-                        .foregroundColor(.black)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 6)
-                                .stroke(Color.gray, lineWidth: 1))
-                }
             }
-            
-            Divider()
-            
-            // post grid view
         }
     }
 }
