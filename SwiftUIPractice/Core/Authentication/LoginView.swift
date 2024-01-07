d58ee5f8869b6128ac6a8e608950299e000af9c0
suojae3 <suojae3@gmail.com>
2024-01-07 17:16:01 +0900

    feat: LoginView 추가

---
+++ SwiftUIPractice/Core/Authentication/LoginView.swift
@@ -0,0 +1,109 @@
+//
+//  SwiftUIView.swift
+//  SwiftUIPractice
+//
+//  Created by ㅣ on 1/7/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+                Spacer()
+                
+                // logo Image
+                Image("TastyCodeLogo")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 100)
+                
+                // text fields
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .autocapitalization(.none)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray5))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    TextField("Enter your password", text: $password)
+                        .autocapitalization(.none)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray5))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                Button {
+                    print("Show forgot password")
+                } label: {
+                    Text("Forgot password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 44)
+                        .cornerRadius(10)
+                        .background(Color(.systemBlue))
+                        .cornerRadius(8)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                    
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                }
+                .foregroundColor(.gray)
+                
+                HStack {
+                    Image("AppleLoginLogo")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                    
+                    Text("Continue with Apple Email")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(.systemBlue))
+                }
+                .padding(.top, 8)
+             
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink {
+                    Text("Signup View")
+                } label: {
+                    HStack(spacing: 5) {
+                        Text("Don't have an account?")
+                      
+                        Text("Sign up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical, 6)
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
