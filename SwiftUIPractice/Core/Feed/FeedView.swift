bff63ca1f671577fa97b57b0c9b4e05b0571aa6d
suojae3 <suojae3@gmail.com>
2024-01-07 15:21:51 +0900

    feat: FeedView 추가

--- SwiftUIPractice/Core/Feed/FeedView.swift
+++ SwiftUIPractice/Core/Feed/FeedView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0...10, id: \.self) { post in
+                        FeedCell()
+                    }
+                }
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("TastyCodeLogo")
+                        .resizable()
+                        .frame(width: 100, height: 30)
+                }
+            }
+        }
     }
 }
 
