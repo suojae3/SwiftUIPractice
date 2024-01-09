126f4f55edc60fd1c25719c45a82f8b39f98b59b
suojae3 <suojae3@gmail.com>
2024-01-09 20:40:57 +0900

    feat: UploadPostViewModel 추가

--- SwiftUIPractice/Core/UploadPost/UploadPostView.swift
+++ SwiftUIPractice/Core/UploadPost/UploadPostView.swift
@@ -12,14 +12,18 @@ struct UploadPostView: View {
     
     @State private var caption = ""
     @State private var imagePickerPresented = false
-    @State private var photoItem: PhotosPickerItem?
+    @StateObject var viewModel = UploadPostViewModel()
+    @Binding var tabIndex: Int
     
     var body: some View {
         VStack {
             // Action tool Bar
             HStack {
                 Button {
-                    print("Cancel upload")
+                    caption = ""
+                    viewModel.selectedImage = nil
+                    viewModel.postImage = nil
+                    tabIndex = 0
                 } label: {
                     Text("Cancel")
                 }
@@ -42,10 +46,15 @@ struct UploadPostView: View {
             .padding(.horizontal)
             
             // Post image and caption
-            HStack {
-                Image("Map")
-                    .resizable()
-                    .frame(width: 100, height: 100)
+            HStack(spacing: 8) {
+                
+                if let image = viewModel.postImage {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                }
                 TextField("Enter your caption...", text: $caption, axis: .vertical)
             }
             
@@ -54,11 +63,10 @@ struct UploadPostView: View {
         .onAppear {
             imagePickerPresented.toggle()
         }
-        .photosPicker(isPresented: $imagePickerPresented, selection: $photoItem)
-        
+        .photosPicker(isPresented: $imagePickerPresented, selection: $viewModel.selectedImage)
     }
 }
 
 #Preview {
-    UploadPostView()
+    UploadPostView(tabIndex: .constant(0))
 }
---
+++ SwiftUIPractice/Core/UploadPost/UploadPostViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  UploadPostViewModel.swift
+//  SwiftUIPractice
+//
+//  Created by ㅣ on 1/9/24.
+//
+
+import SwiftUI
+import PhotosUI
+
+@MainActor
+ class UploadPostViewModel: ObservableObject {
+    
+    @Published var selectedImage: PhotosPickerItem? {
+        didSet { Task { await loadImage(fromItem: selectedImage) } }
+    }
+    
+    @Published var postImage: Image?
+     
+    func loadImage(fromItem item: PhotosPickerItem?) async {
+        guard let item = item else { return }
+        
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: data) else { return }
+        self.postImage = Image(uiImage: uiImage)
+    }
+    
+}
