12f8c158f11e9c6bf9372e440277f443dc9ade76
suojae3 <suojae3@gmail.com>
2024-01-09 21:42:46 +0900

    feat: ContentViewModel 추가

---
+++ SwiftUIPractice/Core/Root/View/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIPractice
+//
+//  Created by ㅣ on 1/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var viewModel = ContentViewModel()
+    
+    var body: some View {
+        Group {
+            if viewModel.userSession == nil {
+                LoginView()
+            } else {
+                MainTabView()
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIPractice/Core/Root/ViewModel/ContentViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ContentViewModel.swift
+//  SwiftUIPractice
+//
+//  Created by ㅣ on 1/9/24.
+//
+
+import Foundation
+import Firebase
+import Combine
+
+class ContentViewModel: ObservableObject {
+    
+    private let service = AuthService.shared
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    init() {
+        
+    } 
+    
+    
+    func setupSubscribers() {
+        service.$userSession.sink { [weak self] userSession in
+            self?.userSession = userSession
+        }
+        .store(in: &cancellables)
+    }
+}
